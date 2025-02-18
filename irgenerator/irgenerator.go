259720220ba8faa6dbb8b1a99c15a35dc4edf3ff
tfhuhtal <tuomas.huhtala@icloud.com>
2025-02-18 14:41:07 +0200

    fix ci

--- irgenerator/irgenerator.go
+++ irgenerator/irgenerator.go
@@ -1,1 +1,13 @@
 package irgenerator
+
+import (
+	"compiler/ast"
+	"compiler/typechecker"
+)
+
+// represent the name of a memory location or built-in
+type IRVar struct {
+	name string
+}
+
+func Generate(rootExpr ast.Expression)
